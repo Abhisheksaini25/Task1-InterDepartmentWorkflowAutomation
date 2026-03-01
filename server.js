12d64f6b29b011cda094b526cb0eddfd15e04bf2
Abhisheksaini25 <abhishek0saini7@gmail.com>
2026-03-01 20:06:59 +0530

    render

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const app=require('./app')
+const app = require('./app')
 require("dotenv").config();
-const connectDb=require('./src/config/db')
-const usermodel=require('./src/models/user')
-const port =process.env.PORT
+const connectDb = require('./src/config/db')
+const usermodel = require('./src/models/user')
+const port = process.env.PORT || 3000
 const bcrypt = require("bcryptjs");
 
 const startServer = async () => {
   await connectDb();
-  
+
   app.listen(port, () => console.log(`Server started at ${port}`));
 };
 
